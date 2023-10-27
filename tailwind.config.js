7e0406c7be9074ecf0badd39fbdd86d3543d4781
antidoid <singhpiyush998@gmail.com>
2023-10-27 17:47:26 +0530

    Add: Background

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "product-background":
+          "url('https://images.unsplash.com/photo-1682685797828-d3b2561deef4?auto=format&fit=crop&q=80&w=3540&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+      },
+    },
+  },
+  plugins: [],
+};
