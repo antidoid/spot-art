fdf2ca41962c45b71a72a53a13ae90243a26ff68
antidoid <singhpiyush998@gmail.com>
2023-10-28 16:41:54 +0530

    Add: Emoji to welcome

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Particles, Card, Form } from "./components";
+import smile from "./assets/smile.svg";
 
 export default function App() {
   return (
@@ -7,7 +8,12 @@ export default function App() {
       <main className="w-[95%] sm:max-w-2xl h-screen font-inter flex flex-col justify-center shadow-2xl">
         <h1 className="font-mono text-white text-3xl ml-4 mb-4">SpotArt</h1>
         <Form />
-        <Card>Welcome to SpotArt</Card>
+        <Card>
+          <h2 className="text-white text-3xl sm:text-5xl">
+            Welcome to SpotArt
+          </h2>
+          <img src={smile} className="w-[250px] mt-10" />
+        </Card>
       </main>
     </div>
   );
