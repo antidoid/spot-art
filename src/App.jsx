5753b2f43bf1df00842307d3336fe70917b632c8
antidoid <singhpiyush998@gmail.com>
2023-10-28 14:47:20 +0530

    Feat: Form Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Particles, Card, Form } from "./components";
 
 export default function App() {
   return (
-    <main className="h-screen bg-product-background bg-bottom bg-cover bg-[#696969] bg-blend-multiply flex flex-col items-center justify-center">
+    <main className="h-screen font-inter bg-product-background bg-bottom bg-cover bg-[#696969] bg-blend-multiply flex flex-col items-center justify-center">
       <Particles />
       <Form />
       <Card>Welcome to SpotArt</Card>
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,21 @@
+import { useState } from "react";
+
 export default function Form() {
+  const [name, setName] = useState(undefined);
+
   return (
-    <div className="h-16 w-[95%] sm:max-w-2xl backdrop-blur-md bg-white/20 rounded-3xl"></div>
+    <div className="h-16 w-[95%] sm:max-w-2xl backdrop-blur-md bg-white/20 rounded-3xl flex items-center">
+      <input
+        type="text"
+        onChange={(e) => setName(e.target.value)}
+        value={name}
+        placeholder="Enter an artist name..."
+        autoFocus
+        className="w-3/5 text-xl ml-auto bg-transparent focus:outline-none text-white sm:text-4xl"
+      />
+      <button className="w-24 h-3/5 mr-4 bg-[#6A6A6A] text-md sm:text-lg uppercase rounded-2xl hover:btn-hover">
+        Search
+      </button>
+    </div>
   );
 }
