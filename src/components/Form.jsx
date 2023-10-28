16740f6218c0353fbce27ccdfb63e8958d93c793
antidoid <singhpiyush998@gmail.com>
2023-10-28 14:59:07 +0530

    Fix: Form component to handle enter keystrok

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,18 @@
 import { useState } from "react";
 
 export default function Form() {
-  const [name, setName] = useState(undefined);
+  const [name, setName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name);
+  };
 
   return (
-    <div className="h-16 w-[95%] sm:max-w-2xl backdrop-blur-md bg-white/20 rounded-3xl flex items-center">
+    <form
+      onSubmit={handleSubmit}
+      className="h-16 w-[95%] sm:max-w-2xl backdrop-blur-md bg-white/20 rounded-3xl flex items-center"
+    >
       <input
         type="text"
         onChange={(e) => setName(e.target.value)}
@@ -13,9 +21,12 @@ export default function Form() {
         autoFocus
         className="w-3/5 text-xl ml-auto bg-transparent focus:outline-none text-white sm:text-4xl"
       />
-      <button className="w-24 h-3/5 mr-4 bg-[#6A6A6A] text-md sm:text-lg uppercase rounded-2xl hover:btn-hover">
+      <button
+        onClick={handleSubmit}
+        className="w-24 h-3/5 mr-4 bg-[#6A6A6A] text-md sm:text-lg uppercase rounded-2xl hover:btn-hover"
+      >
         Search
       </button>
-    </div>
+    </form>
   );
 }
