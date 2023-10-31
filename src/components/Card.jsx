d0faa77d45a0960c2ce654292d7bf708f481cc56
antidoid <singhpiyush998@gmail.com>
2023-10-31 15:16:59 +0530

    Fix: Margins

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 export default function Card({ children }) {
   return (
-    <div className="min-h-[67%] w-full mt-5 rounded-[30px] backdrop-blur-md bg-white/20 flex flex-col items-center">
+    <div className="min-h-[67%] w-full mt-5 mb-8 rounded-[30px] backdrop-blur-md bg-white/20 flex flex-col items-center">
       {children}
     </div>
   );
