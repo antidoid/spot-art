c45f87003139dfdf35dbd4c081b292eca86d7085
antidoid <singhpiyush998@gmail.com>
2024-01-26 15:13:25 +0530

    Fix: Finally fix the z-index bug

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ export default function App() {
     <div className="w-screen flex justify-center bg-product-background bg-bottom bg-cover bg-[#696969] bg-blend-multiply">
       <main className="w-[95%] sm:max-w-2xl min-h-screen font-inter flex flex-col justify-center shadow-2xl">
         <h1
-          className="font-mono font-bold text-white text-4xl m-4 cursor-pointer z-1"
+          className="font-mono font-bold text-white text-4xl m-4 cursor-pointer fix-canvas-bug"
           onClick={handleLogoClick}
         >
           SpotArt
